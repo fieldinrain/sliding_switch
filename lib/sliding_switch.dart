e5f620e95dd9fa0dcd45f4252ef5408f4e7c6e60
fieldinrain <fieldinrain@gmail.com>
2021-04-22 15:17:27 +0900

    apply null-safety

--- lib/sliding_switch.dart
+++ lib/sliding_switch.dart
@@ -13,13 +13,13 @@ class SlidingSwitch extends StatefulWidget {
   final Color background;
   final Color buttonColor;
   final Color inactiveColor;
-  final Function onTap;
-  final Function onDoubleTap;
-  final Function onSwipe;
+  final Function? onTap;
+  final Function? onDoubleTap;
+  final Function? onSwipe;
 
   const SlidingSwitch({
-    @required this.value,
-    @required this.onChanged,
+    required this.value,
+    required this.onChanged,
     this.height = 55,
     this.width = 250,
     this.animationDuration = const Duration(milliseconds: 400),
@@ -38,13 +38,12 @@ class SlidingSwitch extends StatefulWidget {
   _SlidingSwitch createState() => _SlidingSwitch();
 }
 
-class _SlidingSwitch extends State<SlidingSwitch>
-    with SingleTickerProviderStateMixin {
-  AnimationController animationController;
-  Animation<double> animation;
+class _SlidingSwitch extends State<SlidingSwitch> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
   double value = 0.0;
 
-  bool turnState;
+  late bool turnState;
 
   @override
   void dispose() {
@@ -55,13 +54,9 @@ class _SlidingSwitch extends State<SlidingSwitch>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-        vsync: this,
-        lowerBound: 0.0,
-        upperBound: 1.0,
-        duration: widget.animationDuration);
-    animation =
-        CurvedAnimation(parent: animationController, curve: Curves.easeInOut);
+    animationController =
+        AnimationController(vsync: this, lowerBound: 0.0, upperBound: 1.0, duration: widget.animationDuration);
+    animation = CurvedAnimation(parent: animationController, curve: Curves.easeInOut);
     animationController.addListener(() {
       setState(() {
         value = animation.value;
@@ -76,22 +71,20 @@ class _SlidingSwitch extends State<SlidingSwitch>
     return GestureDetector(
         onDoubleTap: () {
           _action();
-          if (widget.onDoubleTap != null) widget.onDoubleTap();
+          if (widget.onDoubleTap != null) widget.onDoubleTap!();
         },
         onTap: () {
           _action();
-          if (widget.onTap != null) widget.onTap();
+          if (widget.onTap != null) widget.onTap!();
         },
         onPanEnd: (details) {
           _action();
-          if (widget.onSwipe != null) widget.onSwipe();
+          if (widget.onSwipe != null) widget.onSwipe!();
         },
         child: Container(
           height: widget.height,
           width: widget.width,
-          decoration: BoxDecoration(
-              color: widget.background,
-              borderRadius: BorderRadius.circular(50)),
+          decoration: BoxDecoration(color: widget.background, borderRadius: BorderRadius.circular(50)),
           padding: EdgeInsets.all(2),
           child: Stack(children: <Widget>[
             Transform.translate(
@@ -117,9 +110,7 @@ class _SlidingSwitch extends State<SlidingSwitch>
                     child: Text(
                       widget.textOff,
                       style: TextStyle(
-                          color: turnState
-                              ? widget.inactiveColor
-                              : widget.colorOff,
+                          color: turnState ? widget.inactiveColor : widget.colorOff,
                           fontSize: 17,
                           fontWeight: FontWeight.w600),
                     ),
@@ -130,8 +121,7 @@ class _SlidingSwitch extends State<SlidingSwitch>
                     child: Text(
                       widget.textOn,
                       style: TextStyle(
-                          color:
-                              turnState ? widget.colorOn : widget.inactiveColor,
+                          color: turnState ? widget.colorOn : widget.inactiveColor,
                           fontSize: 17,
                           fontWeight: FontWeight.w600),
                     ),
@@ -150,9 +140,7 @@ class _SlidingSwitch extends State<SlidingSwitch>
   _determine({bool changeState = false}) {
     setState(() {
       if (changeState) turnState = !turnState;
-      (turnState)
-          ? animationController.forward()
-          : animationController.reverse();
+      (turnState) ? animationController.forward() : animationController.reverse();
       if (changeState) widget.onChanged(turnState);
     });
   }
